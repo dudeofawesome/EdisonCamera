4b1e22879e7d4d645114d3cd5e5e025ff2f7b610
Louis Orleans <louis@0rleans.com>
2015-04-13 02:45:28 -0600

    bulb mode works now

--- Server/edison_camera.js
+++ Server/edison_camera.js
@@ -1,10 +1,36 @@
-var buttonController = require("./modules/button_controller.js");
+var buttonController = require("button_controller");
 var cameraController = require("./modules/camera_controller.js");
 
-buttonController.setTriggerDownCallback(function () {
-    console.log("shooting for 1000ms");
-    cameraController.fireBulb(1000);
-}).setAFdownCallback(function () {
-    console.log("AFing");
-    cameraController.autoFocus();
-});
+var BUTTON_TRIGGER_PIN = 3;
+var BUTTON_AF_PIN = 2;
+
+
+var shootingBulbMode = false;
+buttonController.add(BUTTON_TRIGGER_PIN,
+	null,
+	null,
+	function () {
+		console.log("shooting in bulb mode");
+		cameraController.openShutter();
+		shootingBulbMode = true;
+	},
+	function () {
+		if (!shootingBulbMode) {
+			console.log("shooting for 100ms");
+			cameraController.fireBulb(100);
+		} else {
+			cameraController.closeShutter();
+			shootingBulbMode = false;
+		}
+	});
+
+buttonController.add(BUTTON_AF_PIN,
+	null,
+	null,
+	null,
+	function () {
+		console.log("AFing");
+		cameraController.autoFocus();
+	});
+
+buttonController.setLongClickTime(500);
